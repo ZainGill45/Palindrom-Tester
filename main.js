f5e2a62fb4d869ba958434aa792a93da06b6aa6a
Zain <zainworking45@gmail.com>
2022-02-15 18:04:26 -0700

    Implemented color change on success or failure made it responsive and did error handling

--- main.js
+++ main.js
@@ -1,44 +1,62 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 let testIfWordIsPalindrom = () => {
   // Get input string
-  let word = palindromInput.value;
+  let input = palindromInput.value;
 
-  if (word === '') {
+  if (input === '' || isNumeric(input)) {
+    palindromInput.value = null;
     outputMessage.innerHTML = `This Is Not A Valid Input`;
+    console.log('here');
     return false
   }
 
-  let originalInput = word;
+  let originalInput = input;
   palindromInput.value = null;
 
-  // We want to split up the word into it's characters so they can be indexed also need to format the text string
-  word = formatPalindromString(word);
+  // We want to split up the input into it's characters so they can be indexed also need to format the text string
+  input = input.replaceAll(' ', '').replaceAll(',', '').replaceAll('.', '').toLowerCase().split("");
 
   // Then we want to test if the first and last words are the same
-  while (word[0] === word[word.length - 1]) {
-    word.shift();
-    word.pop();
-    if (word.length === 0 || word.length === 1) {
+  while (input[0] === input[input.length - 1]) {
+    input.shift();
+    input.pop();
+    if (input.length === 0 || input.length === 1) {
       outputMessage.innerHTML = `${originalInput} Is A Palindrom`; // console.log("This is a palindrom");
+      container.style.backgroundColor = isPalindromBackgroundColor;
       break;
     }
   }
 
-  if (word.length > 2) {
+  if (input.length > 2) {
     outputMessage.innerHTML = `${originalInput} Is Not A Palindrom`; // console.log("Not a palindroms");
+    container.style.backgroundColor = notPalindromBackgroundColor;
     isPaldindrom = false;
   }
 }
 
-
-let formatPalindromString = (word) => {
-  word = word.replaceAll(' ', '');
-  word = word.replaceAll(',', '');
-  word = word.replaceAll('.', '');
-  word = word.toLowerCase();
-  word = word.split("");
-  return word;
+const isNumeric = (str) => {
+  for (var i = 0; i < str.length; i++) {
+    for (var x = 0; x < numbers.length; x++) {
+      if (parseFloat(str[i]) === numbers[x]) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
+const SetRandomBackgroundColor = () => {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+}
+
+const notPalindromBackgroundColor = "#ee5253";
+const isPalindromBackgroundColor = "#10ac84";
+const container = document.querySelector("body");
 const palindromSubmitBtn = document.querySelector("#palindrom-button").addEventListener('click', testIfWordIsPalindrom);
 const palindromInput = document.querySelector("#palindrom-input");
 const outputMessage = document.querySelector("#output-message");
